8762d75b7cf87664cd4d973ccd6516c21f71f565
bednarowicz <bednarowiczr@gmail.com>
2023-09-06 23:34:17 +0200

    Pieresze otwieranie przeglądarek w selenium

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,16 +1,28 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
 
-
-
-    }
     @Test
-    public void sampleTest(){
-        System.out.println("Hello!");
+    public void openGooglePage(){
+        WebDriver driver = WebDriverFinal("MS Edge");
+        driver.get("https://www.google.com");
+    }
+    public WebDriver WebDriverFinal(String name) throws IllegalArgumentException {
+        switch(name){
+            case "MS Edge":
+                System.setProperty("webdriver.edge.driver", "C:\\Users\\Rafi\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+                return new EdgeDriver();
+            case "FireFox":
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\Rafi\\Downloads\\geckodriver-v0.33.0-win64 (1)\\geckodriver.exe");
+                return new FirefoxDriver();
+            case "Chrome":
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rafi\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+                return new ChromeDriver();
+        }
+        throw new IllegalArgumentException("Podano nie poprawna przegladarke.");
     }
 }
