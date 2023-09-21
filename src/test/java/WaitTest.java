4e8daeadcaf03f1bb06bc6d4966d984d2b6a000e
bednarowicz <bednarowiczr@gmail.com>
2023-09-21 20:00:23 +0200

    selenium wait

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -9,24 +10,71 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class WaitTest {
+    WebDriver driver;
+
     @Test
     public void waitTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
-;
+        ;
         driver.findElement(By.id("clickOnMe")).click();
         //Thread.sleep(5000);
         //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         FluentWait<WebDriver> wait = new FluentWait<>(driver);
         wait.ignoring(NoSuchElementException.class);
         wait.withTimeout(Duration.ofSeconds(10));
-        wait.pollingEvery(Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("pa")));
-        driver.findElement(By.cssSelector("p"));
+        wait.pollingEvery(Duration.ofSeconds(1));
+        //WebElement para = driver.findElement(By.cssSelector("pa"));
+        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+        // driver.findElement(By.cssSelector("p"));
+
+        waitForElementToExist(By.cssSelector("p"));
+    }
+
+    /*public void waitForElementToExist(By locator){
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1));
+
+        wait.until(new Function<WebDriver, Boolean>() {
+            @Override
+                    public Boolean apply(WebDriver driver){
+                List<WebElement> elements = driver.findElements(locator);
+                if (elements.size()>0){
+                    System.out.println("Element jest na stronie");
+                    return true;
+                } else {
+                    System.out.println("Elementu nie ma na stronie");
+                    return false;
+                }
+            }
+        });
+    }*/
+    public void waitForElementToExist(By locator) {
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1));
+
+        wait.until((driver) -> {
+                    List<WebElement> elements = driver.findElements(locator);
+                    if (elements.size() > 0) {
+                        System.out.println("Element jest na stronie");
+                        return true;
+                    } else {
+                        System.out.println("Elementu nie ma na stronie");
+                        return false;
+                    }
+                }
+        );
     }
 }
+
