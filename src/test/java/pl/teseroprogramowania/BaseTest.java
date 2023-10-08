b1bf0e0561f0a9a530cf01fe4a07eb031ca0be12
bednarowicz <bednarowiczr@gmail.com>
2023-10-08 12:52:25 +0200

    before & after etc for @test

--- src/test/java/pl/teseroprogramowania/BaseTest.java
+++ src/test/java/pl/teseroprogramowania/BaseTest.java
@@ -1,9 +1,24 @@
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+package pl.teseroprogramowania;
+
+import org.testng.annotations.*;
 
 public class BaseTest {
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Before suite.");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before class.");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After suite.");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After class.");
+    }
     @BeforeTest
     public void beforeTest(){
         System.out.println("I am running before test");
--- src/test/java/pl/teseroprogramowania/FirstTest.java
+++ src/test/java/pl/teseroprogramowania/FirstTest.java
@@ -1,19 +1,17 @@
+package pl.teseroprogramowania;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 public class FirstTest extends BaseTest {
     WebDriver driver;
