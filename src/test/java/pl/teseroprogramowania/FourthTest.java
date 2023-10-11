91db6107433d9e836e8f7e49523014604de657f7
bednarowicz <bednarowiczr@gmail.com>
2023-10-11 20:32:40 +0200

    Data provider

--- src/test/java/pl/teseroprogramowania/FourthTest.java
+++ src/test/java/pl/teseroprogramowania/FourthTest.java
@@ -1,5 +1,6 @@
 package pl.teseroprogramowania;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FourthTest extends BaseTest {
@@ -20,5 +21,16 @@ public class FourthTest extends BaseTest {
     public void thirdTest() {
         System.out.println("I am third test.");
         
+    }
+    @Test(dataProvider = "data")
+    public void dpTest(String val, String number) {
+        System.out.println(val);
+        System.out.println(number);
+        
+    }
+
+    @DataProvider(name = "data")
+    public Object[][] dataProvider(){
+        return new Object[][]{{"I am first test", "First."},{"I am second test", "Second"},{"I am third test.", "Third"}};
     }
 }
